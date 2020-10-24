2e4eb020986c3c500ef74c05c53b6500ecd15851
mdshahansha <mdshahansha44411@gmail.com>
2020-10-24 20:50:47 +0530

    added  bit header file in code

--- CPP_Programming/CheckPrime.cpp
+++ CPP_Programming/CheckPrime.cpp
@@ -2,17 +2,22 @@
 
 #include<iostream>
 using namespace std;
-
-int main()
+int prime(int n)
 {
-	int n,i,flag=0;
-	cout<<"Enter any number: ";
-	cin>>n;
-	for (i=2; i<=n/2; i++) {
-		if (n%i==0) 
+    int flag=0;
+    for (int i=2; i<=n/2; i++) {
+		if (n%i==0)
 			flag=1;
 	}
-	if (flag==0)
+	return flag;
+}
+int main()
+{
+	int n,ans=0;
+	cout<<"Enter any number: ";
+	cin>>n;
+
+	if (prime(n))
 		cout<<n<<" is a prime number.";
 	else
 		cout<<n<<" is a composite number.";
